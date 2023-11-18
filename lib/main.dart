54ff30febb688867e715ca47cafb8b0db5151c6a
SrFebry <febryanmdp@mhs.mdp.ac.id>
2023-11-18 18:16:15 +0700

    Navigasi Routing
    Melanjutkabn P-19 Navigasi dan routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:prj_wisatacandisi51_flutterpert3/data/candi_data.dart';
+import 'package:prj_wisatacandisi51_flutterpert3/screens/favorite_screen.dart';
 import 'package:prj_wisatacandisi51_flutterpert3/screens/home_screen.dart';
 import 'package:prj_wisatacandisi51_flutterpert3/screens/search_screen.dart';
 import 'package:prj_wisatacandisi51_flutterpert3/widgets/detail_screen.dart';
@@ -7,7 +10,6 @@ import 'package:prj_wisatacandisi51_flutterpert3/widgets/profile_info_item.dart'
 import 'package:prj_wisatacandisi51_flutterpert3/widgets/profile_screen.dart';
 import 'package:prj_wisatacandisi51_flutterpert3/widgets/signin_screen.dart';
 import 'package:prj_wisatacandisi51_flutterpert3/widgets/signup_screen.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -23,21 +25,91 @@ class MyApp extends StatelessWidget {
       title: 'Wisata Candi',
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
-          iconTheme: IconThemeData(color : Colors.deepPurple),
+          iconTheme: IconThemeData(color: Colors.deepPurple),
           titleTextStyle: TextStyle(
             color: Colors.deepPurple,
             fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
         ),
-        colorScheme: 
-        ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
           primary: Colors.deepPurple,
           surface: Colors.deepPurple[50],
         ),
         useMaterial3: true,
       ),
-      home: HomeScreen(),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  // TODO : 1. Deklarasikan variabel
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomeScreen(),
+    SearchScreen(),
+    FavoriteScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: Theme(
+        data:
+            Theme.of(context).copyWith(canvasColor: Colors.deepPurple.shade50),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.deepPurple,
+              ),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: Colors.deepPurple,
+              ),
+              label: "Search",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.red,
+              ),
+              label: "Favorite",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: Colors.deepPurple,
+              ),
+              label: "Person",
+            ),
+          ],
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.deepPurple[100],
+          showUnselectedLabels: true,
+        ),
+      ),
     );
   }
 }
