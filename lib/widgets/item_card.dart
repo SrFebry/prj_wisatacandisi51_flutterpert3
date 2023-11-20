b734e67523371cd54c2707685368a724a823a59e
SrFebry <febryanmdp@mhs.mdp.ac.id>
2023-11-20 19:52:06 +0700

    Interaksi Pengguna
    Materi PPT P-20 Interaksi Pengguna

--- lib/widgets/item_card.dart
+++ lib/widgets/item_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:prj_wisatacandisi51_flutterpert3/models/candi.dart';
+import 'package:prj_wisatacandisi51_flutterpert3/widgets/detail_screen.dart';
 
 class ItemCard extends StatelessWidget {
   // TODO : 1. DEKLARASIKAN VARIABEL
@@ -10,50 +11,61 @@ class ItemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //TODO : 6. Implementasi ROUTING KE DETAILSCREEN
+    
 
-    return Card(
-      // TODO : 2. tetapkan parameter
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      margin: EdgeInsets.all(4),
-      elevation: 1,
-
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // TODO : 3. Buat IMAGE
-          Expanded(
-            // TODO : 7. Implementasi Hero Animation
-
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: Image.asset(
-                candi.imageAsset,
-                width: double.infinity,
-                fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context, 
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(candi: candi)
+            ),
+        );
+      },
+      child: Card(
+        // TODO : 2. tetapkan parameter
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        margin: EdgeInsets.all(4),
+        elevation: 1,
+    
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // TODO : 3. Buat IMAGE
+            Expanded(
+              // TODO : 7. Implementasi Hero Animation
+    
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset(
+                  candi.imageAsset,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          // TODO : 4. BUAT TEXT
-          Padding(
-            padding: const EdgeInsets.only(left: 16, top: 8),
-            child: Text(
-              candi.name,
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ),
-          // TODO : 5. BUAT TEXT
-          Padding(
-            padding: const EdgeInsets.only(left: 16, bottom: 8),
-            child: Text(
-              candi.type,
-              style: TextStyle(
-                fontSize: 12,
+            // TODO : 4. BUAT TEXT
+            Padding(
+              padding: const EdgeInsets.only(left: 16, top: 8),
+              child: Text(
+                candi.name,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-        ],
+            // TODO : 5. BUAT TEXT
+            Padding(
+              padding: const EdgeInsets.only(left: 16, bottom: 8),
+              child: Text(
+                candi.type,
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
