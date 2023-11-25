7dda20569754558af29f0793be7214fb1b15d673
SrFebry <febryanmdp@mhs.mdp.ac.id>
2023-11-25 18:34:44 +0700

    Validasi Password
    P-21 validasi password di ppt discord

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,8 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: MainScreen(),
+      //home: MainScreen(),
+      home: SignUpScreen(),
     );
   }
 }
--- lib/widgets/signup_screen.dart
+++ lib/widgets/signup_screen.dart
@@ -2,114 +2,101 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatefulWidget {
-  SignUpScreen({super.key});
+  const SignUpScreen({super.key});
 
   @override
-  State<SignUpScreen> createState() => _SignInScreenState();
+  State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _SignInScreenState extends State<SignUpScreen> {
-  // TODO : 1. Deklarasikan Variabel
-  final TextEditingController _usernamecontroller = TextEditingController();
-  final TextEditingController _namacontroller = TextEditingController();
-  final TextEditingController _passwordcontroller = TextEditingController();
+class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
-  String _errorText = '';
-
-  bool _isSignedIn = false ;
-
+  String _errorText = "";
   bool _obscurePassword = true;
+
+  void _signUp() {
+    final String name = _namaController.text.trim();
+    final String username = _usernameController.text.trim();
+    final String password = _passwordController.text.trim();
+
+    if (password.length < 8 ||
+        !password.contains(RegExp(r'[A-Z]')) ||
+        !password.contains(RegExp(r'[a-z]')) ||
+        !password.contains(RegExp(r'[0-9]')) ||
+        !password.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+      setState(() {
+        _errorText =
+            'Minimal 8 karakter, kombinasi [A-Z] ,kombinasi [a-z] , [0-9],!@#%^&*(),.?":{}|<> ';
+      });
+      return;
+    }
+
+    print('**Sign Up berhasil**');
+    print('Nama : $name');
+    print('Nama pengguna: $username');
+    print('Password : $password');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // TODO : 2. Pasang AppBar
-      appBar: AppBar(title : Text ('Sign In'),),
-      // TODO : 3. Pasang Body
-      body: Center(
-        child: SingleChildScrollView(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
           padding: const EdgeInsets.all(16),
           child: Form(
             child: Column(
-              // TODO : 4. Atur main axisAligment dan cross axis aligment
-              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                // TODO : 5. Textformfield nama Lengkap
                 TextFormField(
-                  controller: _namacontroller,
+                  controller: _namaController,
                   decoration: InputDecoration(
                     labelText: "Nama",
-                    border: OutlineInputBorder(),       
+                    border: OutlineInputBorder(),
                   ),
                 ),
-                // TODO : 5. Textformfield nama pengguna
-                SizedBox(height: 20),
+                SizedBox(
+                  height: 20,
+                ),
                 TextFormField(
-                  controller: _usernamecontroller,
+                  controller: _usernameController,
                   decoration: InputDecoration(
-                    labelText: "Nama Pengguna",
-                    border: OutlineInputBorder(),       
-                  ),
+                      labelText: "Nama Pengguna", border: OutlineInputBorder()),
                 ),
-
-                
-                // TODO : 6. Textformfield kata sandi
-                SizedBox(height: 20),
+                SizedBox(
+                  height: 20,
+                ),
                 TextFormField(
-                  controller: _passwordcontroller,
+                  controller: _passwordController,
                   decoration: InputDecoration(
                     labelText: "Kata Sandi",
-                    errorText: _errorText.isNotEmpty ? _errorText : null,
                     border: OutlineInputBorder(),
+                    errorText: _errorText.isNotEmpty ? _errorText : null,
                     suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _obscurePassword = !_obscurePassword;
-                        });
-                      }, 
-                      icon: Icon(
-                        _obscurePassword ? Icons.visibility_off : Icons.visibility,
-                      ),
-                    ),
+                        onPressed: () {
+                          setState(() {
+                            _obscurePassword = !_obscurePassword;
+                          });
+                        },
+                        icon: Icon(_obscurePassword
+                            ? Icons.visibility_off
+                            : Icons.visibility)),
                   ),
                   obscureText: _obscurePassword,
                 ),
-                // TODO : 7. elevatedbutton sign in
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {}, 
-                  child: Text('Sign In'),
-                  ),
-                
-                // TODO : 8. elevatedbutton sign Up
-                SizedBox(height: 10),
-                // TextButton(
-                //   onPressed: () {}, 
-                //   child: Text('Belum Punya Akun? Daftar di sini.')
-                //   ),
-        
-                RichText(
-                  text: TextSpan(
-                    text: 'Belum Punya Akun ? ',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.deepPurple  
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: 'Daftar Di Sini',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
-                          fontSize: 16,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {}
-                      ),
-                    ],
-                  ),
+                const SizedBox(
+                  height: 20,
                 ),
+                ElevatedButton(
+                    onPressed: () {
+                      _signUp();
+                    },
+                    child: const Text('Sign Up')),
               ],
             ),
           ),
