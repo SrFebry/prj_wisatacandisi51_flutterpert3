ed0edf5376585fced70a908a72e50119fbb61683
SrFebry <febryanmdp@mhs.mdp.ac.id>
2023-12-04 20:37:16 +0700

    adding encryption and decryption

--- lib/widgets/signin_screen.dart
+++ lib/widgets/signin_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 
 class SignInScreen extends StatefulWidget {
   SignInScreen({super.key});
@@ -19,19 +20,81 @@ class _SignInScreenState extends State<SignInScreen> {
   bool _isSignedIn = false;
   bool _obscurePassword = false;
 
-  void _signIn() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String savedUsername = prefs.getString('username') ?? '';
-    final String savedPassword = prefs.getString('password') ?? '';
-    final String enteredUsername = _usernameController.text.trim();
-    final String enteredPassword = _passwordController.text.trim();
+  Future<Map<String, String>> _retrieveAndDecryptDataFromPrefs(
+    Future<SharedPreferences> prefs,
+  ) async {
+    final sharedPreferences = await prefs;
+    final encryptedUserName = sharedPreferences.getString('username') ?? '';
+    final encryptedPassword = sharedPreferences.getString('password') ?? '';
+    final keyString = sharedPreferences.getString('key') ?? '';
+    final ivString = sharedPreferences.getString('iv') ?? '';
 
-    if (enteredUsername.isEmpty || enteredPassword.isEmpty) {
-      setState(() {
-        _errorText = 'Nama pengguna dan kata sandi harus diisi.';
-      });
-      return;
-    }
+    final encrypt.Key key = encrypt.Key.fromBase64(keyString);
+    final iv = encrypt.IV.fromBase64(ivString);
+
+    final encrypter = encrypt.Encrypter(encrypt.AES(key));
+    final decryptedUsername = encrypter.decrypt64(encryptedUserName, iv: iv);
+    final decryptedPassword = encrypter.decrypt64(encryptedPassword, iv: iv);
+
+    // Mengembalikan data terdekripsi
+    return {'username': decryptedUsername, 'password': decryptedPassword};
+  }
+
+  void _signIn() async {
+    try {
+      final Future<SharedPreferences> prefsFuture =
+      SharedPreferences.getInstance();
+
+      final String username = _usernameController.text;
+      final String password = _passwordController.text;
+      print('Sign in attempt');
+
+      if (username.isNotEmpty && password.isNotEmpty) {
+        final SharedPreferences prefs = await prefsFuture;
+        final data = await _retrieveAndDecryptDataFromPrefs(prefs);
+        if (data.isNotEmpty) {
+          final decryptedUsername = data['username'];
+          final decryptedPassword = data['password'];
+        
+          if (username == decryptedUsername && password == decryptedPassword) {
+            _errorText = '';
+            _isSignedIn = true;
+        prefs.setBool('isSignedIn', true);
+        // Pemanggilan untuk menghapus semua halaman dalam tumpukan navigasi
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.of(context).popUntil((route) => route.isFirst);
+        }); // Sign in berhasil, navigasikan ke layar utama
+ WidgetsBinding.instance.addPostFrameCallback((_) {
+ Navigator.pushReplacementNamed(context, '/');
+ });
+ print('Sign in succeeded');
+ } else {
+ print('Username or password is incorrect');
+ }
+ } else {
+ print('No stored credentials found');
+ }
+ } else {
+ print('Username and password cannot be empty');
+ // Tambahkan pesan untuk kasus ketika username atau password kosong
+ }
+ } catch (e) {
+ print('An error occurred: $e');
+ }
+}
+  // void _signIn() async {
+  //   final SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   final String savedUsername = prefs.getString('username') ?? '';
+  //   final String savedPassword = prefs.getString('password') ?? '';
+  //   final String enteredUsername = _usernameController.text.trim();
+  //   final String enteredPassword = _passwordController.text.trim();
+
+  //   if (enteredUsername.isEmpty || enteredPassword.isEmpty) {
+  //     setState(() {
+  //       _errorText = 'Nama pengguna dan kata sandi harus diisi.';
+  //     });
+  //     return;
+  //   }
 
     if (savedUsername.isEmpty || savedPassword.isEmpty) {
       setState(() {
--- lib/widgets/signup_screen.dart
+++ lib/widgets/signup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -35,15 +36,30 @@ class _SignUpScreenState extends State<SignUpScreen> {
       });
       return;
     }
+    if (name.isNotEmpty && username.isNotEmpty && password.isNotEmpty) {
+      final encrypt.Key key = encrypt.Key.fromLength(32);
+      final iv = encrypt.IV.fromLength(16);
 
-    // print('**Sign Up berhasil**');
-    // print('Nama : $name');
-    // print('Nama pengguna: $username');
-    // print('Password : $password');
+      final encrypter = encrypt.Encrypter(encrypt.AES(key));
+      final encryptedName = encrypter.encrypt(name, iv: iv);
+      final encryptedUsername = encrypter.encrypt(username, iv: iv);
+      final encryptedPassword = encrypter.encrypt(password, iv: iv);
 
-    prefs.setString("fullname", name);
-    prefs.setString("username", username);
-    prefs.setString("password", password);
+      // print('**Sign Up berhasil**');
+      // print('Nama : $name');
+      // print('Nama pengguna: $username');
+      // print('Password : $password');
+
+      prefs.setString("fullname", encryptedName.base64);
+      prefs.setString("username", encryptedUsername.base64);
+      prefs.setString("password", encryptedPassword.base64);
+      prefs.setString("key", key.base64);
+      prefs.setString("iv", iv.base64);
+
+      //prefs.setString("fullname", name);
+      // prefs.setString("username", username);
+      // prefs.setString("password", password);
+    }
 
     Navigator.pushReplacementNamed(context, '/signin');
   }
